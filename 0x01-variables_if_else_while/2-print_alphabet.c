76c89ee1246a493a6ee0c280d178e839ebc82926
Your Name <you@example.com>
2023-07-17 14:00:58 -0700

    hi

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar('a' + i);
+}
+putchar('\n');
+return (0);
+}
