eaa1695e8ff63ac04068ba5702baba5cbad01e14
Your Name <you@example.com>
2023-08-28 14:21:18 -0700

    hi

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,22 @@
 #include "lists.h"
 /**
- * print_listint - prints a linked lists
- * @h: pointer to frist node 
- *return : size of list 
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
  */
-size_t print_listint(const listint_t *h);
+
+size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
 	while (h)
 	{
-		printf("%d\n",h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
+
 	return (i);
 }
 
