aa6367dde6b55740320059346202d933fe46fa9d
Your Name <you@example.com>
2023-08-28 14:08:03 -0700

    hi

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ *return : size of list 
+ */
+
+size_t print_listint(const listint_t *h);
+{
+	size_t i=0;
+	while (h)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
