75d9f13530a3964182589f9b3a86f756f2c0fc4d
Your Name <you@example.com>
2023-09-08 17:17:39 -0700

    hi

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,82 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+/**
+ * check_elf - Checks if a file is an ELF file
+ * @e_ident: a pointer to an array containing the ELF magic numbers
+ * Description: If the file is not an ELF file - exit code 98
+ */
+void check_elf(unsigned char *e_ident)
+{
+	int ind;
+
+	for (ind = 0; ind < 4; ind++)
+	{
+		if (e_ident[index] != 127 &&
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
+/**
+ *print_magic - a Prints the magic numbers of an elf header
+ *@e_ident: a pointer to an array containing the elf magic numbers
+ *Description: Magic numbers are separated by spaces
+ */
+void print_magic(unsigned char *e_ident)
+{
+	int ind;
+
+	printf("  Magic:   ");
+
+	for (ind = 0; ind < EI_NIDENT; ind++)
+	{
+		printf("%02x", e_ident[ind]);
+
+		if (ind == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
+/**
+ *print_class - Prints the class of an elf header
+ *@e_ident:a pointer to an array containing the ELF class
+ */
+void print_class(unsigned char *e_ident)
+{
+	printf("  Class:   		");
+	switch (e_ident[EI_CLASS])
+	{
+	case ELFCLASSNONE:
+		printf("none\n");
+		break;
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
